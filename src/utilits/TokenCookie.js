2405e4aede3cd1bf24dc58775fb1245354035611
ikshimul <ikshimuluits@gmail.com>
2023-09-04 19:40:18 +0600

    cookie httponly remove issuer change

--- src/utilits/TokenCookie.js
+++ src/utilits/TokenCookie.js
@@ -3,6 +3,6 @@ import { CreateToken } from "./JWTHelper";
 export async function TokenCookie(email) {
   let token = await CreateToken(email);
   return {
-    "Set-Cookie": `token=${token}; Max-Age=7200; Secure; HttpOnly; Path=/; SameSite=Strict`,
+    "Set-Cookie": `token=${token}; Max-Age=7200; Secure; Path=/; SameSite=Strict`,
   };
 }
